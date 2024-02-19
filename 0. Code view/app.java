e882035727c8f759816b18b7ed626f6d92e54d43
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-02-19 13:45:44 +0530

    Add files via upload

---
+++ 0. Code view/app.java
@@ -0,0 +1,8 @@
+//Overall Code schema in Java Programming Language. /Source Code 
+class app {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        // or another logics
+    }
+}
---
+++ 1. Variables and Data Types/app.java
@@ -0,0 +1,27 @@
+//Normal Syntax in Java
+/*class Variables {
+    public static void main(String args[]) {
+        System.out.println("Hello Coders "); // Output Statement
+    }
+}*/
+
+//Variables:
+/*class variables {
+    public static void main(String[] args) {
+        int numOne = 10; // Define a variable
+        int numTwo = 10; 
+        System.out.println("Sum Of the two Number is: " + (numOne + numTwo));
+    }
+}*/
+
+//Inputs in Java
+import java.util.*;
+
+class Varibes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        String Output = sc.nextLine();
+        System.out.println(Output);
+    }
+}

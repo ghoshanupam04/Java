f347bfe634344a9219931659ad920f6859c656c9
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-02-22 11:47:00 +0530

    Add files via upload

---
+++ 4. Loops/app.java
@@ -0,0 +1,39 @@
+//for loops:
+class Loops {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) { // here is the condition
+            System.out.println("Hello Coders"); // print this three time.
+        }
+    }
+}
+
+// Using the for loop print 0-10 numbers:
+class Loops {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// While Loops:
+class Loops {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + ", ");
+            i++;
+        }
+    }
+}
+
+// Do-while loop:
+class Loops {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.print(i + ", ");
+            i++;
+        } while (i <= 10);
+    }
+}
